9f454ab271267938790b895fc22d3855272b944e
Kevin Bradley <kevin.w.bradley@me.com>
2020-04-19 21:06:10 -0700

    minor changes

--- make.sh
+++ make.sh
@@ -9,8 +9,8 @@ mkdir -p $APPS
 
 pushd AirDropHelper
 rm -rf build
-/usr/bin/xcodebuild BUILD_ROOT=../build | xcpretty
-ldid2 -Sent.plist build/Release-appletvos/AirDropHelper.app/AirDropHelper
+xcodebuild BUILD_ROOT=../build | xcpretty
+ldid -Sent.plist build/Release-appletvos/AirDropHelper.app/AirDropHelper
 rm -rf build/Release-appletvos/AirDropHelper.app/embedded.mobileprovision
 rm -rf build/Release-appletvos/AirDropHelper.app/_CodeSignature
 cp -r build/Release-appletvos/AirDropHelper.app ../layout/Applications/
@@ -21,6 +21,6 @@ SDK_PATH="`xcrun --sdk appletvos --show-sdk-path`"
 pushd breezyd
 xcrun -sdk appletvos clang -v -isysroot $SDK_PATH -arch arm64 -Iinclude -F. -framework Foundation -framework TVServices -framework Sharing -mappletvos-version-min=9.0 -o breezyd breezyd.m
 
-ldid2 -Sent.plist breezyd
+ldid -Sent.plist breezyd
 cp breezyd ../layout/usr/bin/
 popd
